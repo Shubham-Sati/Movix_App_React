67e49b30e21afd53b2ffe4e2889c96248306ac99
Shubham Sati <68639424+Shubham-Sati@users.noreply.github.com>
2023-04-21 11:28:55 +0530

    HomePage Done

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,7 @@ function App() {
 
   useEffect(() => {
     fetchApiConfig();
+    genresCall();
   }, []);
 
   const fetchApiConfig = () => {
@@ -31,6 +32,24 @@ function App() {
 
       dispatch(getApiConfiguration(url));
     });
+  };
+
+  const genresCall = async () => {
+    let promises = [];
+    let endPoints = ["tv", "movie"];
+    let allGenres = {};
+
+    endPoints.forEach((url) => {
+      promises.push(fetchDataFromApi(`/genre/${url}/list`));
+    });
+
+    const data = await Promise.all(promises);
+    console.log(data);
+    data.map(({ genres }) => {
+      return genres.map((item) => (allGenres[item.id] = item));
+    });
+
+    dispatch(getGenres(allGenres));
   };
 
   return (
--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -6,24 +6,23 @@ import SwitchTabs from "../../../components/switchTabs/SwitchTabs";
 
 import useFetch from "../../../hooks/useFetch";
 
-const Popular = () => {
-  const [endpoint, setEndpoint] = useState("movie");
-
-  const { data, loading } = useFetch(`/${endpoint}/popular`);
+const Trending = () => {
+  const [endpoint, setEndpoint] = useState("day");
+  const { data, loading } = useFetch(`/trending/all/${endpoint}`);
 
   const onTabChange = (tab) => {
-    setEndpoint(tab === "Movies" ? "movie" : "tv");
+    setEndpoint(tab === "Day" ? "day" : "week");
   };
 
   return (
     <div className="carouselSection">
       <ContentWrapper>
-        <span className="carouselTitle">What's Popular</span>
-        <SwitchTabs data={["Movies", "TV Shows"]} onTabChange={onTabChange} />
+        <span className="carouselTitle">Trending</span>
+        <SwitchTabs data={["Day", "Week"]} onTabChange={onTabChange} />
       </ContentWrapper>
-      <Carousel data={data?.results} loading={loading} endpoint={endpoint} />
+      <Carousel data={data?.results} loading={loading} />
     </div>
   );
 };
 
-export default Popular;
+export default Trending;
